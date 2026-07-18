f636c204bf43b02e8f1692c7e5e249c82e0eb472
muslimbekuz <muslimbekkhalimjonov@gmail.com>
2026-07-18 16:47:19 +0900

    feat: develop AppRouteState and HomePage type integrations

---
+++ src/lib/types/screen.ts
@@ -0,0 +1,14 @@
+import { Member } from "./member";
+import { Product } from "./product";
+
+/* REACT APP STATE */
+export interface AppRootState {
+  homePage: HomePageState;
+}
+
+/* HOME PAGE */
+export interface HomePageState {
+  popularDishes: Product[];
+  newDishes: Product[];
+  topUsers: Member[];
+}
