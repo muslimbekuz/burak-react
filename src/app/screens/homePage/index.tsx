861e59bc435af7e14e0e4da10092faee44819e63
muslimbekuz <muslimbekkhalimjonov@gmail.com>
2026-07-21 14:04:40 +0900

    feat: create config & env variables

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -26,7 +26,7 @@ const popularDishesRetriever = createSelector(
 export default function HomePage() {
   const { setPopularDishes } = actionDispatch(useDispatch());
   const { popularDishes } = useSelector(popularDishesRetriever);
-  // Selector: Store => Data
+
   useEffect(() => {}, []);
 
   return (
