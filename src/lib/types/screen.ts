e416330aa5e751d17f899481915299daf3366c0b
muslimbekuz <muslimbekkhalimjonov@gmail.com>
2026-07-31 18:30:47 +0900

    feat: develop OrdersPage redux configuration

--- src/lib/types/screen.ts
+++ src/lib/types/screen.ts
@@ -1,10 +1,12 @@
 import { Member } from "./member";
+import { Order } from "./order";
 import { Product } from "./product";
 
 /* REACT APP STATE */
 export interface AppRootState {
   homePage: HomePageState;
   productsPage: ProductsPageState;
+  ordersPage: OrdersPageState;
 }
 
 /* HOME PAGE */
@@ -19,4 +21,11 @@ export interface ProductsPageState {
   restaurant: Member | null;
   chosenProduct: Product | null;
   products: Product[];
+}
+
+/* ORDERS PAGE */
+export interface OrdersPageState {
+  pausedOrders: Order[];
+  processOrders: Order[];
+  finishedOrders: Order[];
 }
